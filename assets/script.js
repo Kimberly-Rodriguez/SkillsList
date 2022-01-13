c4def0f9d3522fd2dbc2ea2232ca25b763293d2e
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2022-01-12 17:33:47 -0800

    created fetch request to the github url

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,93 @@
-var formEl = $('#skills-form');
-var nameInputEl = $('#skill-name');
-var dateInputEl = $('#datepicker');
-var skillsListEl = $('#skills-list');
+$(document).ready(function () {
+  let formEl = $("#skills-form");
+  let nameInputEl = $("#skill-name");
+  let dateInputEl = $("#datepicker");
+  let skillsListEl = $("#skills-list");
+  let tableBody = $("#repo-table");
+  let fetchButton = $("#fetch-button");
 
-// creating a funtion that dinamically creates a list of skills that have a name and date
-var printSkills = function (name, date) {
-  var listEl = $('<li>');
-  var listDetail = name.concat(' on ', date);
-  listEl.addClass('list-group-item').text(listDetail);
-  listEl.appendTo(skillsListEl);
-};
+  // creating a funtion that dinamically creates a list of skills that have a name and date
+  let printSkills = function (name, date) {
+    let listEl = $("<li>");
+    let listDetail = name.concat(" on ", date);
+    listEl.addClass("list-group-item").text(listDetail);
+    listEl.appendTo(skillsListEl);
+  };
 
-// creating an event prevent default for the input form 
-var handleFormSubmit = function (event) {
-  event.preventDefault();
+  // creating an event prevent default for the input form
+  let handleFormSubmit = function (event) {
+    event.preventDefault();
 
-  // getting the value for name and date and setting them up as nameInput & dateInput
-  var nameInput = nameInputEl.val();
-  var dateInput = dateInputEl.val();
- 
-  // creating and if statment to check if name/date input have been sumbited if funtion is returned
-  if (!nameInput || !dateInput) {
-    console.log('You need to fill out the form!');
-    return;
-  }
- 
-  // name/date input are being past to the printSkills function
-  printSkills(nameInput, dateInput);
- // then name/date inputEl is being cleared so that next input start fresh
-  nameInputEl.val('');
-  dateInputEl.val('');
-};
+    // getting the value for name and date and setting them up as nameInput & dateInput
+    let nameInput = nameInputEl.val();
+    let dateInput = dateInputEl.val();
 
-formEl.on('submit', handleFormSubmit);
+    // creating and if statment to check if name/date input have been sumbited if funtion is returned
+    if (!nameInput || !dateInput) {
+      console.log("You need to fill out the form!");
+      return;
+    }
 
-// Autocomplete widget
-$(function () {
-  var skillNames = [
-    'Bootstrap',
-    'C',
-    'C++',
-    'CSS',
-    'Express.js',
-    'Git',
-    'HTML',
-    'Java',
-    'JavaScript',
-    'jQuery',
-    'JSON',
-    'MySQL',
-    'Node.js',
-    'NoSQL',
-    'PHP',
-    'Python',
-    'React',
-    'Ruby',
-  ];
-  $('#skill-name').autocomplete({
-    source: skillNames,
+    // name/date input are being past to the printSkills function
+    printSkills(nameInput, dateInput);
+    // then name/date inputEl is being cleared so that next input start fresh
+    nameInputEl.val("");
+    dateInputEl.val("");
+  };
+
+  formEl.on("submit", handleFormSubmit);
+
+  // Autocomplete widget
+  $(function () {
+    let skillNames = [
+      "Bootstrap",
+      "C",
+      "C++",
+      "CSS",
+      "Express.js",
+      "Git",
+      "HTML",
+      "Java",
+      "JavaScript",
+      "jQuery",
+      "JSON",
+      "MySQL",
+      "Node.js",
+      "NoSQL",
+      "PHP",
+      "Python",
+      "React",
+      "Ruby",
+    ];
+    $("#skill-name").autocomplete({
+      source: skillNames,
+    });
+  });
+
+  // Datepicker widget
+  $(function () {
+    $("#datepicker").datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
+
+  // Add interaction here
+  $(function () {
+    $("#skills-list").sortable({
+      placeholder: "ui-state-highlight",
+    });
+    $("#skills-list").disableSelection();
+  });
+
+  fetchButton.on("click", async () => {
+    let requestUrl = "https://api.github.com/orgs/nodejs/repos";
+    let response = await fetch(requestUrl);
+
+    response = await response.json();
+    response.forEach((data) => {
+      let url = data.html_url;
+      tableBody.append(`<tr><td><a href="${url}">${url}</a></td></tr>`);
+    });
   });
 });
-
-// Datepicker widget
-$(function () {
-  $('#datepicker').datepicker({
-    changeMonth: true,
-    changeYear: true,
-  });
-});
-
-// Add interaction here
-$( function() {
-  $( "#skills-list" ).sortable({
-    placeholder: "ui-state-highlight"
-  });
-  $( "#skills-list" ).disableSelection();
-} );
-
-
